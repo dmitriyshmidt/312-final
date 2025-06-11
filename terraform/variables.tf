434fe696d4aff59d6a08d29ff965abd2113ca011
Dmitriy Shmidt <83924497+dmitriyshmidt@users.noreply.github.com>
2025-06-10 17:06:35 -0700

    Create variables.tf
    Created variables.tf file with initial code

---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  description = "The AWS region to deploy into"
+  type = string
+  default = "us-east-1"
+}
+
+variable "key_name" {
+  description = "Name of the existing AWS EC2 Key pair to use for SSH access"
+  type = string
+}
+
+variable "vpc_id" {
+    description = "The ID of the VPC where the instance will be deployed"
+    type = string
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet to launch the instance into"
+  type = string
+}
