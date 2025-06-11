a3d40501f8e314070f8919c134f806f5c2bb35bd
Dmitriy Shmidt <83924497+dmitriyshmidt@users.noreply.github.com>
2025-06-10 19:59:53 -0700

    Update setup_mc.sh
    Updated script

--- scripts/setup_mc.sh
+++ scripts/setup_mc.sh
@@ -1,1 +1,43 @@
+#!/bin/bash
 
+# Update packages and install Java
+sudo yum update -y
+sudo amazon-linux-extras enable corretto8
+sudo yum install -y java-1.8.0-amazon-corretto
+
+# Create a minecraft user
+sudo useradd -m -r -d /opt/minecraft minecraft
+
+# Create server directory
+sudo mkdir -p /opt/minecraft/server
+sudo chown minecraft:minecraft /opt/minecraft/server
+cd /opt/minecraft/server
+
+# Download Minecraft server jar
+sudo -u minecraft curl -o server.jar https://launcher.mojang.com/v1/objects/3e7fbe9c404c4fa2dc6fba1c6fd8e0338c388c89/server.jar
+
+# Accept EULA
+echo "eula=tru" | sudo tee /opt/minecraft/server/euala.txt
+
+# Create a systemd service
+sudo tee /etc/systemd/system/minecraft.service > /dev/null <<EOL
+[Unit]
+Desecription=Minecraft Server
+After=network.target
+
+[Service]
+User=minecraft
+Nice=1
+WorkingDirectory=/opt/minecraft/server
+ExecStart=/usr/bin/java -Xmx1G -jar server.jar nogui
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Enable and start the service
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl enable minecraft
+sudo systemctl start minecraft
